9721d0a6d1fe00c7692003102240e408f63ecbc3
jonnylee007 <jonnylee007@gmail.com>
2023-04-10 23:43:47 +0800

    mouse

--- js/common.js
+++ js/common.js
@@ -1,3 +1,22 @@
+// ============ mouse trailer ============
+const trailer = document.getElementById("trailer");
+
+window.onmousemove = e => {
+    const x = e.clientX - trailer.offsetWidth / 2;
+    const y = e.clientY - trailer.offsetHeight / 2;
+
+    const keyframes = {
+        transform: `translate(${x}px, ${y}px)`
+    }
+
+    trailer.animate(keyframes, {
+        duration: 800,
+        fill: "forwards"
+    })
+}
+
+
+// ============ RWD_menu ============
 $(function(){
     $('.toggle').click(function(){
         $('li').toggleClass('hidden');
